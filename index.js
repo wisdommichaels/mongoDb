a11dba6eda8802d98b00282d055c4cf6b1f30251
wisdom michael <ijarwisdom@gmail.com>
2024-08-19 05:21:10 -0700

    added functions

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ dotenv.config()
 
 const uri = process.env.MONGO_URI
 const client = new MongoClient(uri)
+const dbname = "bible_quotes"
+
+const connectToMyDataBase = async () {
+    try {
+        await client.connect();
+        console.log("Connected to MongoDB")
+    }
+    catch (err) {
+        console.error("Failed to connect to MongoDB", err)
+    } finally {
+        await client.close();
+    }
+}    
