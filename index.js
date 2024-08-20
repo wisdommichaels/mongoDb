318754396982331a1fd3789dde496cb081c83026
wisdom michael <ijarwisdom@gmail.com>
2024-08-20 06:29:04 -0700

    added quotes to my db

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ dotenv.config()
 const uri = process.env.MONGO_URI
 const client = new MongoClient(uri)
 const dbname = "bible_quotes"
+const bibleQuotesCollection = client.db(dbname).collection("love_quotes")
 
 const connectToMyDataBase = async() => {
     try {
@@ -13,16 +14,26 @@ const connectToMyDataBase = async() => {
     }
     catch (err) {
         console.error("Failed to connect to MongoDB", err)
-    } finally {
-        await client.close();
-    }
-}    
+} 
+}   
+
+const quote = [
+    {
+        newBook : "1 Peter 4:8",
+        newQuote: "Above all, love each other deeply, because love covers over a multitude of sins."
+    },
+    {
+        newBook : "Romans 5:5",
+        newQuote :"And hope does not put us to shame, because God’s love has been poured out into our hearts through the Holy Spirit, who has been given to us."
+        
+    },
+]
 
 const main = async() => {
     try {
         await connectToMyDataBase();
-        // const db = client.db(dbname);
-        // const collection = db.collection("bible_quotes");
+       let result = await bibleQuotesCollection.insertMany(quote)
+       console.log(result);
     } catch (error) {
         console.error("Error occurred during the execution", error);
     }finally {
